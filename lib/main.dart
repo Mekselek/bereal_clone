e5223c54b47b6c203d33e4dcafdf517f7945474c
Maksymilian <maksymilian.kwasek@gmail.com>
2023-01-14 12:57:19 +0100

    Added Top Appbar and user photos

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,11 +13,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-    
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData(scaffoldBackgroundColor: Colors.black),
+      darkTheme: ThemeData.dark(),
       home: const MyHomePage(),
     );
   }
@@ -26,6 +27,105 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        body: SafeArea(
+      child: Column(
+        children: [
+          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Transform.scale(
+                scaleX: -1,
+                child: Icon(
+                  Icons.people_alt,
+                  color: Colors.white,
+                  size: 30,
+                ),
+              ),
+            ),
+            Text(
+              'BeReal.',
+              style: GoogleFonts.roboto(
+                  textStyle: TextStyle(
+                      color: Colors.white,
+                      fontSize: 28,
+                      fontFamily: 'Genera',
+                      fontWeight: FontWeight.w800)),
+            ),
+            Container(
+              width: 30,
+              height: 30,
+              child: CircleAvatar(
+                backgroundImage: AssetImage('lib/assets/avatar.jpeg'),
+              ),
+            ),
+          ]),
+          Padding(
+            padding: const EdgeInsets.only(top: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('My Friends',
+                    style: GoogleFonts.roboto(
+                      textStyle: TextStyle(
+                          color: Colors.white,
+                          fontSize: 19,
+                          fontFamily: 'Genera',
+                          fontWeight: FontWeight.w600),
+                    )),
+                SizedBox(
+                  width: 20,
+                ),
+                Text('Discovery',
+                    style: GoogleFonts.roboto(
+                      textStyle: TextStyle(
+                          color: Color.fromARGB(255, 137, 136, 136),
+                          fontSize: 19,
+                          fontFamily: 'Genera',
+                          fontWeight: FontWeight.w600),
+                    )),
+              ],
+            ),
+          ),
+          Column(
+            children: [
+              Column(
+                children: [
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Stack(children: [
+                    Container(
+                        width: 120,
+                        height: 170,
+                        foregroundDecoration: BoxDecoration(
+                          border: Border.all(color: Colors.black),
+                          borderRadius: BorderRadius.all(Radius.circular(15)),
+                          image: DecorationImage(
+                              image: AssetImage('lib/assets/big.jpg'),
+                              fit: BoxFit.fill),
+                        )),
+                    Padding(
+                      padding: const EdgeInsets.all(5.0),
+                      child: Container(
+                        width: 40,
+                        height: 55,
+                        foregroundDecoration: BoxDecoration(
+                          border: Border.all(color: Colors.black),
+                          borderRadius: BorderRadius.all(Radius.circular(9)),
+                          image: DecorationImage(
+                              image: AssetImage('lib/assets/small.jpg'),
+                              fit: BoxFit.fill),
+                        ),
+                      ),
+                    ),
+                  ]),
+                ],
+              )
+            ],
+          )
+        ],
+      ),
+    ));
   }
 }
